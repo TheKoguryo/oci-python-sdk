3b0347d466d6abbf5bbf16e3abb821cce4dc173f
TheKoguryo <thekoguryo@gmail.com>
2023-02-06 01:20:53 +0000

    oci usage update

--- examples/usage_reports_to_adw/usage-for-cost-saving.sh
+++ examples/usage_reports_to_adw/usage-for-cost-saving.sh
@@ -1,3 +1,2 @@
-#python3 usage-for-cost-saving.py -d 2022-10-30 -ts Oracle-Tags.CreatedBy
-python3 usage-for-cost-saving.py -du USAGE -dp We1lc2om3e#45 -dn ADWCUSG_low -d 2022-12-05 -ts Oracle-Tags.CreatedBy
+python3 usage-for-cost-saving.py -d 2023-01-01 -ts Oracle-Tags.CreatedBy
 
